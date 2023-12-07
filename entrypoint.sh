40e6db2f3f097a0ca16673862d4aec1e6cade0bf
Your Name <you@example.com>
2023-12-07 14:15:19 +0000

    new tests

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,8 +1,9 @@
 #!/bin/sh
 
 # Check the environment variable
-if [ "$COMPOSE_TEST" = "TEST" ]; then
+if [ "$COMPOSE_TEST" = "TEST123" ]; then
     echo "Environment is valid. Proceeding..."
+    echo $COMPOSE_TEST
 else
     echo "Environment is not valid. Exiting..."
     exit 1
